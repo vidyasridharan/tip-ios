a96c5514a3d0d49b14336888801bec9ee47ca336
Vidya Sridharan <vidyasridharan@Vidyas-MacBook-Air.local>
2014-09-03 21:22:50 -0700

    Initial Commit

---
+++ tip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tip
+//
+//  Created by Vidya Sridharan on 9/3/14.
+//  Copyright (c) 2014 Vidya Sridharan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tip
+//
+//  Created by Vidya Sridharan on 9/3/14.
+//  Copyright (c) 2014 Vidya Sridharan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
